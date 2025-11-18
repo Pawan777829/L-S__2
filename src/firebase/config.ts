9f9be7047bf5f66338a1125f50f51c8145a63a07
Harshit singh <pawan9915405618@gmail.com>
2025-11-18 05:48:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1815353862-d82a6",
+  "appId": "1:66985967076:web:7886acccb959394a2b1bf1",
+  "apiKey": "AIzaSyBbbWxw2lgD7yyqhYRREtExr0Uqb__ms5Q",
+  "authDomain": "studio-1815353862-d82a6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "66985967076"
+};
